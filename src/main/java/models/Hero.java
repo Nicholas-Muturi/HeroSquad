a05548f2c584378d5b08cc03d7648ae0a1c56d5d
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-12 17:32:49 +0300

    [Hero Test] Instantiation test to check whether hero instantiates correctly with basic properties i.e name, age, strength, weakness passed

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,39 @@
 package models;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,12 +2,28 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("ALL")
 public class HeroTest {
 
     @Before
     public void setUp() throws Exception {
     }
+
+    private Hero setupNewHero(){
+        return new Hero ("Batman",40,"Money","Loneliness");
+    }
+
+    @Test
+    public void heroInstantiatesCorrectly() {
+        Hero newHero = setupNewHero();
+        assertTrue(newHero instanceof Hero);
+    }
+
+
 
     @After
     public void tearDown() throws Exception {
