850fef4bfd26389684e0fd5c2e1cd2fc865ed9a3
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-12 16:20:32 +0300

    [chore] created the HERO and SQUAD classes required by the objectives and also created their own tests. Also imported the required spark dependancies and made handlebar index and layout files.

---
+++ src/test/java/models/SquadTest.java
@@ -0,0 +1,15 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class SquadTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
