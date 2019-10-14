2791181c91bb3f6b6ccfd5957221cce512335538
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-14 16:20:56 +0300

    [chore] heroku deployment preparation. Added PORT configuration in the App class to allow heroku to assign a port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,17 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         //get: retrieve user session in homepage first
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
